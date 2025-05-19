b3939bda90c26882a2878fba195fe37836c530bf
Jaein Lee <jilee0818@gmail.com>
2025-05-19 18:12:22 +0900

    N2748_피보나치수2

--- coding_test/src/week04/N2748_피보나치수2.java
+++ coding_test/src/week04/N2748_피보나치수2.java
@@ -3,18 +3,18 @@ package week04;
 import java.util.Scanner;
 
 public class N2748_피보나치수2 {
-	public static int fibo(int n) {
-		if (n == 0) return 0;
-		else if (n == 1) return 1;
-		else {
-			return fibo(n - 1) + fibo(n - 2);
-		}
-	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		System.out.println(fibo(n));
+		long[] arr = new long[n + 1];
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+		
+		System.out.println(arr[n]);
 	}
 }
